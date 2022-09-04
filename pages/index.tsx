263430acb9846c8465cd310f006ad3b18a4443ac
Aditya Ghaffar <adit.bisa@gmail.com>
2022-09-04 12:21:51 +0700

    refactor: convert javascript to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import React from 'react';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -18,7 +19,7 @@ export default function Home() {
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
