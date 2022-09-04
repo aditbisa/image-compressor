bce00a55339f31671fd00f141bddd30245c4346b
Aditya Ghaffar <adit.bisa@gmail.com>
2022-09-04 14:06:34 +0700

    feat: path alias

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const path = require("node:path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.resolve.alias["@styles"] = path.resolve(__dirname, "styles");
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
