05e35f48a9b4349fcc87101dcd215b5cefa48847
Aditya Ghaffar <adit.bisa@gmail.com>
2022-09-05 08:59:19 +0700

    feat: FileInfo component

---
+++ src/components/FileInfo.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { KeyValueTable } from "@components/KeyValueTable";
+import styles from "@styles/components/FileInfo.module.css";
+
+export interface FileInfoProp {
+  file: File;
+}
+
+/**
+ * File info table.
+ */
+export function FileInfo(prop: FileInfoProp) {
+  const file = prop.file;
+  const fileSizeMb = (file.size / 1024 / 1024).toFixed(2) + " MB";
+  const data = {
+    Filename: file.name,
+    Size: fileSizeMb,
+    Type: file.type,
+  };
+  return <KeyValueTable data={data} />;
+}
